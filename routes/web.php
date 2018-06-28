fef1d87840bca383929a5219fcfd7fc9505b8bbb
stan wielga <svwielga4@gmail.com>
2018-06-27 22:14:13 -0700

    refactored routes, added tasks controller

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,4 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
-
-Route::get('/', function () {
-    $tasks = DB::table('tasks')->get();
-    // return $tasks;
-    return view('welcome', compact('tasks'));
-});
-Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
-    // return $tasks;
-    return view('tasks.index', compact('tasks'));
-});
-Route::get('/tasks/{task}', function ($id) {
-    $task = App\Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
